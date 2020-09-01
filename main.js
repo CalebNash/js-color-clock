8976ceab9bdb3d9d7426af24af734cc718760a30
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-01 15:40:17 -0400

    changes clock updates every second

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+function displayTime()
+{
+  var time = new Date();
+  var hour = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
 
+  const clockDisplay = document.querySelector('.clock-display').textContent = `${hour}:${minutes}:${seconds}`;
+  console.log(clockDisplay);
+}
+const currentClock = setInterval(displayTime, 1000);
