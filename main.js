b98ef9780068dad6eda9820defabdcb6781aaa34
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-01 15:52:06 -0400

    padded clock with zeros

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 function displayTime()
 {
   var time = new Date();
-  var hour = time.getHours();
-  var minutes = time.getMinutes();
-  var seconds = time.getSeconds();
+  var hours = time.getHours().toString();
+  var minutes = time.getMinutes().toString();
+  var seconds = time.getSeconds().toString();
 
-  const clockDisplay = document.querySelector('.clock-display').textContent = `${hour}:${minutes}:${seconds}`;
+if(hours.length < 2)
+{
+  hours = '0' + hours;
+}
+
+if(minutes.length < 2)
+{
+  minutes = '0' + minutes;
+}
+
+if(seconds.length < 2)
+{
+  seconds = '0' + seconds;
+}
+
+  const clockDisplay = document.querySelector('.clock-display').textContent = `${hours}:${minutes}:${seconds}`;
   console.log(clockDisplay);
 }
 const currentClock = setInterval(displayTime, 1000);
